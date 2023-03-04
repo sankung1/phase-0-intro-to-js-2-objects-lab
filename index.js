d2ea817675584084a126184839577217baa20de8
Sankung Sillah <ssillah306@gmail.com>
2023-03-03 21:56:46 -0500

    completed objects lab

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Write your solution in this file!
+
+const employee = {
+    name: "Tim",
+    streetAddress: "Foyt terr"
+}
+
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    const newObject = {...employee};
+    newObject[key] = value;
+
+    return newObject;
+
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){ 
+    employee[key] = value;
+    return employee;
+}
+
+
+function deleteFromEmployeeByKey(employee, key){
+    const newObject = {...employee};
+    delete newObject[key];
+    return newObject;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key];
+    return employee;
+}
